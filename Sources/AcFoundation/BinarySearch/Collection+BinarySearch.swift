7ec84c3da0d0ab26bb9f35d81ecfc0155659e80a
narumij <narumij@gmail.com>
2024-09-23 07:44:34 +0900

    binary search

--- Sources/AcFoundation/BinarySearch/Collection+BinarySearch.swift
+++ Sources/AcFoundation/BinarySearch/Collection+BinarySearch.swift
@@ -2,59 +2,7 @@ import Foundation
 
 // MARK: - Binary Search
 
-extension Range {
-
-  @inlinable
-  func left<Item>(_ x: Item, _ item: (Element) -> Item) -> Element
-  where Item: Comparable, Bound: BinaryInteger {
-    var (left, right) = (startIndex, endIndex)
-    while left < right {
-      let mid = (left + right) >> 1
-      if item(mid) < x {
-        left = mid + 1
-      } else {
-        right = mid
-      }
-    }
-    return left
-  }
-
-  @inlinable
-  func right<Item>(_ x: Item, _ item: (Element) -> Item) -> Element
-  where Item: Comparable, Bound: BinaryInteger {
-    var (left, right) = (startIndex, endIndex)
-    while left < right {
-      let mid = (left + right) >> 1
-      if x < item(mid) {
-        right = mid
-      } else {
-        left = mid + 1
-      }
-    }
-    return left
-  }
-}
-
 extension Collection where Index == Int, Element: Comparable {
-
-  @inlinable
-  func right(_ x: Element, start left: Index, end right: Index) -> Index {
-    var (left, right) = (left, right)
-    while left < right {
-      let mid = (left + right) >> 1
-      if x < self[mid] {
-        right = mid
-      } else {
-        left = mid + 1
-      }
-    }
-    return left
-  }
-
-  @inlinable
-  func right(_ x: Element) -> Index {
-    right(x, start: startIndex, end: endIndex)
-  }
 
   @inlinable
   func left(_ x: Element, start left: Index, end right: Index) -> Index {
@@ -74,17 +22,13 @@ extension Collection where Index == Int, Element: Comparable {
   func left(_ x: Element) -> Index {
     left(x, start: startIndex, end: endIndex)
   }
-}
-
-extension Collection where Index == Int {
 
   @inlinable
-  func right<T>(_ x: T, start left: Index, end right: Index, _ key: (Element) -> T) -> Index
-  where T: Comparable {
-    var (left, right) = (startIndex, endIndex)
+  func right(_ x: Element, start left: Index, end right: Index) -> Index {
+    var (left, right) = (left, right)
     while left < right {
       let mid = (left + right) >> 1
-      if x < key(self[mid]) {
+      if x < self[mid] {
         right = mid
       } else {
         left = mid + 1
@@ -94,17 +38,7 @@ extension Collection where Index == Int {
   }
 
   @inlinable
-  func left<T>(_ x: T, start left: Index, end right: Index, _ key: (Element) -> T) -> Index
-  where T: Comparable {
-    var (left, right) = (left, right)
-    while left < right {
-      let mid = (left + right) >> 1
-      if key(self[mid]) < x {
-        left = mid + 1
-      } else {
-        right = mid
-      }
-    }
-    return left
+  func right(_ x: Element) -> Index {
+    right(x, start: startIndex, end: endIndex)
   }
 }
