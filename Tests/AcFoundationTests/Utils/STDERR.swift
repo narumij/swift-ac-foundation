9fb31fcb94439cac3d08271ba7b7e2672643ff28
narumij <narumij@gmail.com>
2024-11-21 11:20:00 +0900

    make slim

--- Tests/AcFoundationTests/Utils/STDERR.swift
+++ Tests/AcFoundationTests/Utils/STDERR.swift
@@ -1,17 +1,28 @@
-import Foundation
+#if os(macOS) || os(iOS)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(Musl)
+import Musl
+#elseif os(Windows)
+import ucrt
+#else
+//#error(Unknown platform)
+#endif
 
 // MARK: - STDERR
 
-extension UnsafeMutablePointer: TextOutputStream where Pointee == FILE {
-  @inlinable
+extension UnsafeMutablePointer: @retroactive TextOutputStream where Pointee == FILE {
   public mutating func write(_ string: String) {
     guard let data = string.data(using: .utf8) else { return }
     _ = data.withUnsafeBytes { bytes in
-      #if os(Linux)
-        Glibc.write(fileno(self), bytes.baseAddress!, data.count)
-      #else
-        Darwin.write(fileno(self), bytes.baseAddress!, data.count)
-      #endif
+#if os(macOS) || os(iOS)
+      Darwin.write(fileno(self), bytes.baseAddress!, data.count)
+#elseif canImport(Glibc)
+      Glibc.write(fileno(self), bytes.baseAddress!, data.count)
+#elseif canImport(Musl)
+      Musl.write(fileno(self), bytes.baseAddress!, data.count)
+#endif
     }
   }
 }
