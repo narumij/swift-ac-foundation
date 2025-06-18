f0e6133b7bd939fc601a21986a333725b5b2c55c
narumij <narumij@gmail.com>
2025-06-19 02:15:02 +0900

    test

--- Tests/AcFoundationTests/PackTests.swift
+++ Tests/AcFoundationTests/PackTests.swift
@@ -5,35 +5,51 @@
 //  Created by narumij on 2025/06/18.
 //
 
-import XCTest
 import Pack
+import XCTest
 
 final class PackTests: XCTestCase {
 
-    override func setUpWithError() throws { }
+  override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws { }
+  override func tearDownWithError() throws {}
 
-    func testExample() throws {
-      let p: Pack<Int,Int,Int> = .init(1,2,3)
-      XCTAssertEqual(p.tuple.0, 1)
-      XCTAssertEqual(p.tuple.1, 2)
-      XCTAssertEqual(p.tuple.2, 3)
-      let a: [Pack<Int>: Int] = [.init(1): 1]
-      let b: [Pack<Int,Int>: Int] = [.init(1, 1): 1]
-      let c: [Pack<Int,Int,Int>: Int] = [.init(1, 1, 1): 1]
-      let d: [Pack<Int,Int,Int,Int>: Int] = [.init(1, 1, 1, 1): 1]
-      XCTAssertEqual(a[.init(1)], 1)
-      XCTAssertEqual(b[.init(1, 1)], 1)
-      XCTAssertEqual(c[.init(1, 1, 1)], 1)
-      XCTAssertEqual(d[.init(1, 1, 1, 1)], 1)
-      let e: [Pack<Int, UInt, String, Int>: Int] = [.init(1, 1, "", 1): 1]
+  func testExample() throws {
+    let p: Pack<Int, Int, Int> = .init(1, 2, 3)
+    XCTAssertEqual(p.tuple.0, 1)
+    XCTAssertEqual(p.tuple.1, 2)
+    XCTAssertEqual(p.tuple.2, 3)
+    let a: [Pack<Int>: Int] = [.init(1): 1]
+    let b: [Pack<Int, Int>: Int] = [.init(1, 1): 1]
+    let c: [Pack<Int, Int, Int>: Int] = [.init(1, 1, 1): 1]
+    let d: [Pack<Int, Int, Int, Int>: Int] = [.init(1, 1, 1, 1): 1]
+    XCTAssertEqual(a[.init(1)], 1)
+    XCTAssertEqual(b[.init(1, 1)], 1)
+    XCTAssertEqual(c[.init(1, 1, 1)], 1)
+    XCTAssertEqual(d[.init(1, 1, 1, 1)], 1)
+    let e: [Pack<Int, UInt, String, Int>: Int] = [.init(1, 1, "", 1): 1]
+  }
+
+  func testCompare() throws {
+    
+    XCTAssertEqual((1,1) < (1,1), Pack(1,1) < Pack(1,1))
+    XCTAssertEqual((1,2) < (2,1), Pack(1,2) < Pack(2,1))
+    XCTAssertEqual((2,1) < (1,1), Pack(2,1) < Pack(1,1))
+    XCTAssertEqual((2,1) < (2,1), Pack(2,1) < Pack(2,1))
+    XCTAssertEqual((2,2) < (2,2), Pack(2,2) < Pack(2,2))
+    XCTAssertEqual((1,2) < (1,3), Pack(1,2) < Pack(1,3))
+    
+    let a = [(1,1),(1,2),(2,1),(1,3),(3,1),(3,3),(2,3)].sorted(by: <)
+    let b = [(1,1),(1,2),(2,1),(1,3),(3,1),(3,3),(2,3)].map(Pack.init).sorted(by: <)
+    
+    XCTAssertEqual(a.map(\.0), b.map(\.tuple.0))
+    XCTAssertEqual(a.map(\.1), b.map(\.tuple.1))
+  }
+
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  }
 }
