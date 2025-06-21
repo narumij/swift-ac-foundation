8807a229e538c4fdb5bda01c392974fc27103c98
narumij <narumij@gmail.com>
2025-06-22 01:04:15 +0900

    inline

--- Sources/IOReader/Tuple Read.swift
+++ Sources/IOReader/Tuple Read.swift
@@ -4,6 +4,7 @@ public typealias TupleRead = SingleReadable
 
 /// タプルを一括で読みます
 @inlinable
+@inline(__always)
 public func read<each T: TupleRead>() throws -> (repeat each T) {
   (repeat try (each T).read())
 }
@@ -13,6 +14,7 @@ public func read<each T: TupleRead>() throws -> (repeat each T) {
 /// 名前に反して1行を読むわけではないです。
 /// 慣れやすさでこの名前にしています。
 @inlinable
+@inline(__always)
 public func _readLine<each T: TupleRead>() -> (repeat each T)? {
   try? (repeat (each T).read())
 }
