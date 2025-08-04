d3372727b10c10a8ccd03334b4cf5d5ac55be6fe
narumij <narumij@gmail.com>
2025-08-04 23:48:30 +0900

    subscript

--- Sources/StringUtil/String+Subscript.swift
+++ Sources/StringUtil/String+Subscript.swift
@@ -10,7 +10,7 @@ extension String {
 
   @inlinable
   @inline(__always)
-  subscript<R>(bounds: R) -> Substring where R: RangeExpression, R.Bound == Int {
+  public subscript<R>(_subscript bounds: R) -> Substring where R: RangeExpression, R.Bound == Int {
     let bounds = bounds.relative(to: (0..<self.count))
     let start = index(startIndex, offsetBy: bounds.lowerBound)
     let end = index(startIndex, offsetBy: bounds.upperBound)
@@ -20,6 +20,6 @@ extension String {
   @inlinable
   @inline(__always)
   public subscript<R>(bounds: R) -> String where R: RangeExpression, R.Bound == Int {
-    String(self[bounds])
+    String(self[_subscript: bounds])
   }
 }
