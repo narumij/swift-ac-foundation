bbff458db346fe3f27a9e062a1277db08f689d48
narumij <narumij@gmail.com>
2025-08-30 19:35:57 +0900

    stream, test

--- Sources/IOUtil/FileOutputStream.swift
+++ Sources/IOUtil/FileOutputStream.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-public struct FileOutputStream: TextOutputStream {
+/// これが特別速い訳では無く、printと同程度の速度
+public typealias FileOutputStream = FileOutputStream_putchar_unlockd
+
+/// 定数倍の負担がかかる
+public struct FileOutputStream_naive: TextOutputStream {
 
   @usableFromInline let fileHandle: FileHandle
 
@@ -13,28 +17,53 @@ public struct FileOutputStream: TextOutputStream {
   }
 
   nonisolated(unsafe)
-    public static var standardError: FileOutputStream = .init(
-      fileHandle: FileHandle.standardError)
-
-  nonisolated(unsafe)
-    public static var standardOutput: FileOutputStream = .init(
+    public static var standardOutput: FileOutputStream_naive = .init(
       fileHandle: FileHandle.standardOutput)
+  
+  nonisolated(unsafe)
+    public static var standardError: FileOutputStream_naive = .init(
+      fileHandle: FileHandle.standardError)
 }
 
-public struct PutCharUnlockedStream: TextOutputStream {
+/// これが特別速い訳では無く、printと同程度の速度
+public struct FileOutputStream_putchar_unlockd {
   
-  @inlinable
-  @inline(__always)
-  public func write(_ s: String) {
-    if let d = s.data(using: .utf8) {
-      d.withUnsafeBytes {
-        for i in 0..<$0.count {
-          putchar_unlocked(Int32($0[i]))
+  public struct StandardOutput: TextOutputStream {
+    
+    @inlinable
+    @inline(__always)
+    public func write(_ s: String) {
+      if let d = s.data(using: .utf8) {
+        d.withUnsafeBytes {
+          for i in 0..<$0.count {
+            putchar_unlocked(Int32($0[i]))
+          }
         }
       }
     }
   }
+  
+  nonisolated(unsafe)
+    public static var standardOutput: StandardOutput = .init()
+  
+  public struct StandardError: TextOutputStream {
+    
+    @inlinable
+    @inline(__always)
+    public func write(_ s: String) {
+      let backup = stdout
+      stdout = stderr
+      if let d = s.data(using: .utf8) {
+        d.withUnsafeBytes {
+          for i in 0..<$0.count {
+            putchar_unlocked(Int32($0[i]))
+          }
+        }
+      }
+      stdout = backup
+    }
+  }
 
   nonisolated(unsafe)
-    public static var standardOutput: PutCharUnlockedStream = .init()
+    public static var standardError: StandardError = .init()
 }
--- Tests/AcFoundationTests/FastPrintTests.swift
+++ Tests/AcFoundationTests/FastPrintTests.swift
@@ -145,8 +145,63 @@ final class FastPrintTests: XCTestCase {
       """)
   }
   
-  func testSmokeStderr() throws {
-    print("Hello, stderr!", to: &FileOutputStream.standardError)
+  func testFileOutputStream() throws {
+    XCTAssertEqual(
+      try SolverRunner(solver: {
+        // ややおそい
+        "stdout".write(to: &FileOutputStream_naive.standardOutput)
+        // ややおそい
+        "stderr".write(to: &FileOutputStream_naive.standardError)
+      }).run(input:""),
+      """
+      stdout
+      """)
+  }
+
+  func testPutCharUnlockedStream() throws {
+    XCTAssertEqual(
+      try SolverRunner(solver: {
+        // これが特別速い訳では無く、printと同程度の速度
+        "stdout".write(to: &FileOutputStream_putchar_unlockd.standardOutput)
+        // これが特別速い訳では無く、printと同程度の速度
+        "stderr".write(to: &FileOutputStream_putchar_unlockd.standardError)
+      }).run(input:""),
+      """
+      stdout
+      """)
+  }
+  
+  func testInt8() throws {
+    XCTAssertEqual(
+      try SolverRunner(solver: {
+        let a:[Int8] = "Hello".cString(using: .ascii)!
+        fastPrint(a)
+      }).run(input:""),
+      """
+      Hello
+      """)
+  }
+
+  func testUInt8() throws {
+    XCTAssertEqual(
+      try SolverRunner(solver: {
+        let a:[UInt8] = "Hello".compactMap(\.asciiValue)
+        fastPrint(a)
+      }).run(input:""),
+      """
+      Hello
+      """)
+  }
+
+  func testCharacter() throws {
+    XCTAssertEqual(
+      try SolverRunner(solver: {
+        let a:[Character] = Array("Hello")
+        fastPrint(a)
+      }).run(input:""),
+      """
+      Hello
+      """)
   }
 
   func testPerformanceExample() throws {
