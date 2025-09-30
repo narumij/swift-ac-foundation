e556dadfbd213176ad2a567b93afc2aa8eff10be
narumij <narumij@gmail.com>
2025-09-30 17:14:31 +0900

    uint8 string

--- Sources/UInt8Util/UInt8+String.swift
+++ Sources/UInt8Util/UInt8+String.swift
@@ -1,7 +1,14 @@
 import Foundation
 
 extension String {
-  
+
+  @inlinable
+  @inline(__always)
+  public init<S>(asciiValues: S) where S: Sequence, S.Element == UInt8 {
+    self.init(bytes: asciiValues, encoding: .ascii)!
+  }
+
+  @available(*, deprecated, renamed: "init(asciiValues:)")
   @inlinable
   @inline(__always)
   public init<S>(ascii: S) where S: Sequence, S.Element == UInt8 {
