1710eb309287e2d38417413cec26369802f476ba
narumij <narumij@gmail.com>
2025-10-05 03:40:36 +0900

    inline array

--- Tests/AcFoundationTests/ReaderTests+InlineArray.swift
+++ Tests/AcFoundationTests/ReaderTests+InlineArray.swift
@@ -17,9 +17,10 @@ import XCTest
 #endif
 
 @available(macOS 26.0, *)
-extension InlineArray where Element: SingleReadable {
-  @inlinable @inline(__always)
-  public static var stdin: Self { .init { _ in .stdin } }
+extension InlineArray: SingleReadable where Element: SingleReadable {
+  public static func read() throws -> Self {
+    try .init { _ in try Element.read() }
+  }
 }
 
 @available(macOS 26.0, *)
