359d49476fbcf920b8d7196b57313c778e2c8820
narumij <narumij@gmail.com>
2025-10-05 01:49:37 +0900

    inline array

--- Tests/AcFoundationTests/ReaderTests+InlineArray.swift
+++ Tests/AcFoundationTests/ReaderTests+InlineArray.swift
@@ -5,8 +5,8 @@
 //  Created by narumij on 2023/12/18.
 //
 
-import XCTest
 import Foundation
+import XCTest
 
 #if DEBUG
   @testable import IOReader
@@ -20,6 +20,19 @@ import Foundation
 extension InlineArray where Element: SingleReadable {
   @inlinable @inline(__always)
   public static var stdin: Self { .init { _ in .stdin } }
+}
+
+@available(macOS 26.0, *)
+extension InlineArray where Element: SingleReadable {
+  func output() {
+    for i in 0..<endIndex {
+      if i != 0 {
+        print(terminator: " ")
+      }
+      print(self[i], terminator: "")
+    }
+    print(terminator: "\n")
+  }
 }
 
 @available(macOS 26.0, *)
@@ -37,7 +50,7 @@ final class InlineArrayReaderTests: XCTestCase {
     XCTAssertEqual(
       try SolverRunner(solver: {
         let A = [3 of Int].stdin
-        print(A[0], A[1], A[2])
+        A.output()
       })
       .run(
         input:
