ccff71a1ba5641ee5b018cd84e05f0f977d7a65e
narumij <narumij@gmail.com>
2025-12-21 18:09:29 +0900

    array resize

--- Sources/Convenience/Array.swift
+++ Sources/Convenience/Array.swift
@@ -1,14 +1,37 @@
 import Foundation
 
 extension Array where Element: AdditiveArithmetic {
+  
   @inlinable
   public mutating func resize(_ n: Int) {
-    guard count != n else { return }
-    if count > n {
-      removeLast(count - n)
+    resize(n, .zero)
+  }
+
+  @inlinable
+  public func resized(_ n: Int) -> [Element] {
+    var result = self
+    result.resize(n)
+    return result
+  }
+}
+
+extension Array {
+  
+  @inlinable
+  public mutating func resize(_ to: Int, _ element: Element) {
+    guard count != to else { return }
+    if count > to {
+      removeLast(count - to)
       return
     }
-    reserveCapacity(n)
-    append(contentsOf: repeatElement(.zero, count: n - count))
+    reserveCapacity(to)
+    append(contentsOf: repeatElement(element, count: to - count))
+  }
+
+  @inlinable
+  public mutating func resized(_ to: Int, _ element: Element) -> [Element] {
+    var result = self
+    result.resize(to, element)
+    return result
   }
 }
