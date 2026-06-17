0d4fee28c1649c0d6d4ad265da25f425feb21462
narumij <narumij@gmail.com>
2026-06-17 11:00:58 +0900

    fast print

--- Sources/IOUtil/withUnsafeReadLineBytes+CChar.swift
+++ Sources/IOUtil/withUnsafeReadLineBytes+CChar.swift
@@ -1,3 +1,5 @@
+// library-checker用
+
 @preconcurrency import Foundation
 
 @inlinable
@@ -16,65 +18,26 @@ func __readLine_stdin(
 
 @inlinable
 @inline(__always)
-public func __withUnsafeReadLineBytes<T>(_ body: (UnsafeBufferPointer<CChar>) throws -> T) rethrows -> T {
+public func __withUnsafeReadLineBytes<T>(
+  _ body: (UnsafeBufferPointer<UInt8>) throws -> T
+) rethrows -> T {
   var utf8Start: UnsafeMutablePointer<CChar>?
   let utf8Count = __readLine_stdin(&utf8Start)
+
   defer {
     free(utf8Start)
   }
-  return try body(UnsafeBufferPointer(start: utf8Start, count: utf8Count))
-}
 
-@inlinable
-func ___readInt(
-  _ start: UnsafePointer<CChar>,
-  _ count: Int,
-  _ pos: inout Int
-) -> Int {
-
-  while pos < count, start[pos] == 0x20 {
-    pos &+= 1
-  }
-
-  var value = 0
-
-  while true {
-
-    let c = Int(UInt8(bitPattern: start[pos]))
-    pos &+= 1
-
-    if c == 0x20 || c == 0x0A {
-      break
-    }
-
-    value = value &* 10 &+ (c &- 48)
-  }
-
-  return value
-}
-
-@inlinable
-func ___readPair() -> (Int, Int) {
-
-  __withUnsafeReadLineBytes { buffer in
-
-    var pos = 0
-
-    return (
-      ___readInt(buffer.baseAddress!, buffer.count, &pos),
-      ___readInt(buffer.baseAddress!, buffer.count, &pos)
-    )
+  return try utf8Start!.withMemoryRebound(to: UInt8.self, capacity: utf8Count) {
+    try body(UnsafeBufferPointer(start: $0, count: utf8Count))
   }
 }
-
-
-// MARK: -
 
 @inlinable
 @inline(__always)
 public func __withUnsafeReadBytes<T>(
   capacity: Int = 1 << 20,
-  _ body: (UnsafeBufferPointer<CChar>) throws -> T
+  _ body: (UnsafeBufferPointer<UInt8>) throws -> T
 ) rethrows -> T {
   var capacity = capacity
   var count = 0
@@ -87,8 +50,7 @@ public func __withUnsafeReadBytes<T>(
 
   while true {
     if count == capacity {
-      assert(capacity >= 3)
-      let newCapacity = capacity + capacity >> 1
+      let newCapacity = capacity + (capacity >> 1)
       let newBuffer = UnsafeMutablePointer<CChar>.allocate(capacity: newCapacity + 1)
 
       newBuffer.update(from: buffer, count: count)
@@ -109,7 +71,52 @@ public func __withUnsafeReadBytes<T>(
 
   buffer[count] = 0
 
-  return try body(
-    UnsafeBufferPointer(start: buffer, count: count + 1)
-  )
+  return try buffer.withMemoryRebound(to: UInt8.self, capacity: count + 1) {
+    try body(
+      UnsafeBufferPointer(start: $0, count: count + 1)
+    )
+  }
+}
+
+// MARK: -
+
+@inlinable
+@inline(__always)
+func ___readInt(
+  _ start: UnsafePointer<UInt8>,
+  _ count: Int,
+  _ pos: inout Int
+) -> Int {
+  while pos < count, start[pos] == 0x20 {
+    pos &+= 1
+  }
+
+  var value: Int = 0
+
+  while true {
+    let c = start[pos]
+    pos &+= 1
+
+    if c == 0x20 || c == 0x0A {
+      break
+    }
+
+    value = value &* 10 &+ Int(c) &- 48
+  }
+
+  return value
+}
+
+@inlinable
+func ___readPair() -> (Int, Int) {
+
+  __withUnsafeReadLineBytes { buffer in
+
+    var pos = 0
+
+    return (
+      ___readInt(buffer.baseAddress!, buffer.count, &pos),
+      ___readInt(buffer.baseAddress!, buffer.count, &pos)
+    )
+  }
 }
